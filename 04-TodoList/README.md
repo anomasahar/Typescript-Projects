6f63bd072984deab5440a0396a0dfffc13cc070a
anomasahar <anoma.sahar2001@gmail.com>
2024-02-20 14:05:54 +0500

    Todo List Project Completed!🥳

---
+++ 04-TodoList/README.md
@@ -0,0 +1,32 @@
+
+# Todo App 
+    This is a command-line Todo application built with
+    TypeScript, Node.js, and Inquirer. 
+    It allows users to manage their todo list by   
+    providing user friendly experience.
+
+## Key Features:
+🎨  Colorful output using Chalk and Chalk-Animation 
+    for enhanced user experience.
+
+💬 Prompt-based interface using Inquirer for user interaction.
+
+📝 Add, view, update, and delete tasks in your TODO list
+
+## 🔗 Dependencies:
+~ Chalk
+
+~ Inquirer
+
+~ Chalk Animation
+
+~ Boxen 
+
+## Contributing:
+    Contributions are welcome! If you have any 
+    suggestions, bug reports, or feature requests.
+
+
+
+    
+
---
+++ 04-TodoList/index.js
@@ -0,0 +1,105 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import boxen from "boxen";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+const todoList = [];
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    });
+};
+async function welcome() {
+    let rainbowTitle = chalkAnimation.rainbow('✨ Welcome to the TODO App ✨');
+    await sleep();
+    rainbowTitle.stop();
+}
+await welcome();
+async function addTask() {
+    const { task } = await inquirer.prompt({
+        type: 'input',
+        name: 'task',
+        message: chalk.rgb(255, 153, 51)("Enter the list of the tasks you want todo:"),
+    });
+    todoList.push(task);
+    console.log(chalk.green.bold("Task added successfully!"));
+}
+async function viewTasks() {
+    const msg = chalk.underline.bold('Your Todo List:');
+    const tasks = todoList.map((task, index) => chalk.cyan(`${index + 1}. ${task}`)).join('\n');
+    const boxedMessage = boxen(msg + '\n\n' + tasks, { padding: 1, margin: 1, borderStyle: 'double', borderColor: "blue" });
+    console.log(boxedMessage);
+}
+async function updateTask() {
+    const { taskIndex } = await inquirer.prompt({
+        type: 'number',
+        name: 'taskIndex',
+        message: chalk.rgb(255, 153, 51)("Enter the index of the task you want to update:"),
+    });
+    if (taskIndex >= 1 && taskIndex <= todoList.length) {
+        const { newTask } = await inquirer.prompt({
+            type: 'input',
+            name: 'newTask',
+            message: chalk.rgb(255, 153, 51)("Enter the new task:"),
+        });
+        todoList[taskIndex - 1] = newTask;
+        console.log(chalk.green.bold("Task updated successfully!"));
+    }
+    else {
+        console.log(chalk.red.bold("Invalid task index."));
+    }
+}
+async function deleteTask() {
+    const { taskIndex } = await inquirer.prompt({
+        type: 'number',
+        name: 'taskIndex',
+        message: chalk.rgb(255, 153, 51)("Enter the index of the task you want to delete:"),
+    });
+    if (taskIndex >= 1 && taskIndex <= todoList.length) {
+        todoList.splice(taskIndex - 1, 1);
+        console.log(chalk.green.bold("Task deleted successfully!"));
+    }
+    else {
+        console.log(chalk.red.bold("Invalid task index."));
+    }
+}
+async function start() {
+    let exit = false;
+    while (!exit) {
+        const answer = await inquirer.prompt({
+            type: 'list',
+            name: 'list',
+            message: "Which operation would you like to perform?",
+            choices: [
+                "Add",
+                "View",
+                "Update",
+                "Delete", "Exit"
+            ],
+        });
+        switch (answer.list) {
+            case "Add":
+                await addTask();
+                break;
+            case "View":
+                await viewTasks();
+                break;
+            case "Update":
+                await updateTask();
+                break;
+            case "Delete":
+                await deleteTask();
+                break;
+            case "Exit":
+                const msg = chalk.bold(chalk.rgb(204, 102, 153)('Thanks For Using Our Todo App😃'));
+                const boxedMessage = boxen(msg, { padding: 1, margin: 1, borderStyle: 'double', textAlignment: "center", borderColor: "magenta" });
+                console.log(boxedMessage);
+                exit = true;
+                break;
+            default:
+                console.log("Invalid choice");
+                break;
+        }
+    }
+}
+start();
