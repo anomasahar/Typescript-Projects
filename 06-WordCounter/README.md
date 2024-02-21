fe5028d5860f93f127186597c3fc2676147d9ad1
anomasahar <anoma.sahar2001@gmail.com>
2024-02-21 19:56:04 +0500

    Word Counter Project Completed!🥳

---
+++ 06-WordCounter/README.md
@@ -0,0 +1,25 @@
+# Word Counter App 
+    The Word Counter is a simple command-line tool 
+    that counts the number of words in a given  
+    paragraph.    
+
+## Key Features:
+🎨  Colorful output using Chalk and Chalk-Animation 
+    for enhanced user experience.
+
+💬 Prompt-based interface using Inquirer for user interaction.
+
+📝 Simple and intuitive user interface.
+
+## 🔗 Dependencies:
+~ Chalk
+
+~ Inquirer
+
+~ Chalk Animation
+
+~ Boxen 
+
+## Contributing:
+    Contributions are welcome! If you have any 
+    suggestions, bug reports, or feature requests.
---
+++ 06-WordCounter/index.ts
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+import boxen from "boxen";
+import chalk from "chalk";
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    });
+};
+
+async function welcome() {
+    let rainbowTitle = chalkAnimation.rainbow('✨ Welcome to the Word Counter App ✨');
+    await sleep();
+    rainbowTitle.stop();
+}
+await welcome();
+
+async function wordcount():Promise<void>{
+    const answer = await inquirer.prompt({
+        type: 'input',
+        name: 'paragraph',
+        message: 'Enter a paragraph:',
+        validate: (input) => {
+            if (input.trim() === "") {
+            return chalk.red("Please enter a non-empty paragraph.");
+            }
+            return true;
+        },
+    });
+    const paragraph: string = answer.paragraph;
+
+    // Count words (without whitespaces)
+    let paragraphList:string[] = paragraph.trim().split(" ");
+    let wordCounter:number = paragraphList.length;
+
+    const msg = chalk.rgb(255,128,0)(`Total words are:`,wordCounter);
+    const boxedMessage = boxen(msg, 
+        { padding: 1, margin: 1, borderStyle: 'double',textAlignment:"center",borderColor:"white" });
+    console.log(boxedMessage);
+
+    await continueOrExit();
+}
+
+async function continueOrExit(): Promise<void> {
+    const answer = await inquirer.prompt({
+        type: 'list',
+        name: 'continueOrexit',
+        message:'Do you want to continue or exit?',
+        choices: ['Continue', 'Exit']
+    });
+
+    if (answer.continueOrexit === 'Continue') {
+        await wordcount();
+    } 
+    else {
+        const msg = chalk.bold(chalk.rgb(204, 102, 153)('Thanks For Using Our Word Counter App😃'));
+        const boxedMessage = boxen(msg, { padding: 1, margin: 1, borderStyle: 'double', textAlignment: "center",borderColor:"magenta" });
+        console.log(boxedMessage);
+    }
+
+}
+
+await wordcount();
